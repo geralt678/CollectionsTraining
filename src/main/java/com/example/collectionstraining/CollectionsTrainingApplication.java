12b1388558f4c5921977740f18c5e165550e978f
Tomasz Idzior <t.idzior@gmail.com>
2024-01-17 20:13:19 +0100

    Initial commit

---
+++ src/main/java/com/example/collectionstraining/CollectionsTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.collectionstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsTrainingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/collectionstraining/model/Person.java
@@ -0,0 +1,4 @@
+package com.example.collectionstraining.model;
+
+public class Person {
+}
